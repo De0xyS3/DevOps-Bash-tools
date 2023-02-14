ed0d64917763f222acaa70663f2f937d65556f34
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2023-02-13 21:56:12 -0500

    Create nfs-server.sh

---
+++ nfs-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt-get install figlet &> /dev/null
+echo "Comprobando si NFS está instalado..."
+if ! [ -x "$(command -v nfs-kernel-server)" ]; then
+  echo 'NFS no esta instalado. Instalando...'
+  figlet -f slant "Instalando NFS" | pv -qL 10
+    sudo apt update
+    sudo apt install nfs-kernel-server
+  echo "NFS ha sido instalado"
+else
+  echo "NFS ya está instalado."
+fi
+echo "Ingresa ruta que se publicara:"
+read p_dir
+sudo mkdir -p $p_dir
+echo "Estableciendo permisos:"
+sudo chown nobody:nogroup $p_dir
+
+echo "Ingrese IP servidor:"
+read server_ip
+
+cat > /etc/exports << EOL
+$p_dir  $server_ip(rw,sync,no_root_squash,no_subtree_check)
+
+EOL
+
+echo "Reiniciando servicios:"
+sudo systemctl restart nfs-kernel-server
